93a9265e750e53743ad49e93c752dd7ca0b77528
hnthap <94937751+hnthap@users.noreply.github.com>
2024-01-09 16:31:47 +0700

    Remove FileLogger subproject

--- FileLogger/FileLogger.cs
+++ FileLogger/FileLogger.cs
@@ -4,22 +4,29 @@ namespace FileLogger
 {
 	public class FileLogger : ILogger
 	{
-		private string _filePath;
-		private static object _lock = new();
-		
-		public FileLogger(string filePath)
+		private string _filepath;
+
+		public FileLogger(string filepath)
 		{
-			_filePath = filePath;
+			_filepath = filepath;
 		}
 
-		public IDisposable BeginScope<TState>(TState state)
+		public IDisposable? BeginScope<TState>(TState state)
 		{
 			return null;
 		}
 
-		public bool IsEnabled(LogLevel logLevel)
+		public bool IsEnable(LogLevel logLevel)
 		{
 			return true;
+		}
+
+		public void Log<TState>(
+			LogLevel logLevel, EventId eventId, TState state,
+			Exception? exception, Func<TState, Exception?, string> formatter)
+		{
+			string message = formatter(state, exception);
+			File.AppendAllText(_filepath, message + Environment.NewLine);
 		}
 	}
 }
