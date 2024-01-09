14ff90b60d23080386b6c423389e04c24ac03b76
hnthap <94937751+hnthap@users.noreply.github.com>
2024-01-09 16:46:16 +0700

    Add FileLoggingLib subproject to log

---
+++ FileLoggingLib/FileLogger.cs
@@ -0,0 +1,24 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace FileLoggingLib
+{
+	public static class FileLogger
+	{
+		public static string? FilePath = null;
+		public static Logger? Logger = null;
+		public static bool IsInitialized { get { return Logger is not null; } }
+
+		public static void Initialize(string filePath)
+		{
+			var config = new LoggingConfiguration();
+			var logFile = new FileTarget(filePath);
+			config.AddRule(LogLevel.Info, LogLevel.Fatal, logFile);
+			LogManager.Configuration = config;
+			
+			Logger = LogManager.GetCurrentClassLogger();
+			FilePath = filePath;
+		}
+	}
+}
